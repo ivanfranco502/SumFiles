d2ba5953f2c5e3a97d4d5ea8707fc0351ce5fb75
Iván Franco <ivan.franco@betssongroup.com>
2021-11-14 13:23:02 +0100

    initial commit

---
+++ SumFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
